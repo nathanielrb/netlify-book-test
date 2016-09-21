61ad4dab3a1781aac61799358670d4ba24df23d7
nathaniel <nathaniel@marginalia.be>
2016-09-21 16:28:52 +0200

    1

---
+++ admin/load.js
@@ -0,0 +1,24 @@
+var parseQueryString = function() {
+    
+    var str = window.location.search;
+    var objURL = {};
+    
+    str.replace(
+	new RegExp( "([^?=&]+)(=([^&]*))?", "g" ),
+	function( $0, $1, $2, $3 ){
+	    objURL[ $1 ] = $3;
+	}
+    );
+    return objURL;
+};
+
+var params = parseQueryString();
+var book = params["b"];
+
+
+if(!book)
+    alert("no book chosen.")
+
+
+
+document.write("<base href='/netbook/" + book + "/' />");
